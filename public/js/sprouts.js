0fa1680d96ee18c820d92af01e1c2b31d60da5fd
jrpespisa <jrpespisa@verizon.net>
2015-12-03 14:09:38 -0500

    initial commit

---
+++ public/js/sprouts.js
@@ -0,0 +1,16 @@
+var pageNum = 1;
+$('.more-sprouts').on('click', function(event) {
+  event.preventDefault();
+  pageNum += 1;
+
+  var request = $.ajax({
+    method: "GET",
+    url: "/tweets.json?page=" + pageNum
+  });
+
+  request.done(function(data) {
+      for(var i=0;i<data.length;i++){
+      $(".tweets").append("<li class='tweet'><div class='body'>"+data[i]["text"]+'</div><div class="user">'+data[i]["username"]+'</div></li>');
+      }
+  });
+});
